9af186ea3c1d2d9ce1a6df0a6d949dcb64b84784
TajBanana <ytahjian@gmail.com>
2021-12-13 22:53:19 +0800

    basic files for socket programming

---
+++ src/main/java/SingleServer.java
@@ -0,0 +1,9 @@
+public class SingleServer {
+    public static void main(String[] args) {
+        method1();
+    }
+
+    public static void method1() {
+        System.out.println("this is a program");
+    }
+}
--- src/main/java/TestStaticMethod.java
+++ src/main/java/TestStaticMethod.java
@@ -4,14 +4,14 @@ public class TestStaticMethod {
     static String input;
 
     public static void main(String[] args) {
-        scanInputs();
+        scanInput();
         System.out.println(input + " printed by main");
 
         changeInput();
         System.out.println("input " + input);
     }
 
-    public static void scanInputs () {
+    public static void scanInput () {
         Scanner scan = new Scanner(System.in);
         input = scan.nextLine();
         System.out.println(input);
@@ -20,4 +20,5 @@ public class TestStaticMethod {
     public static void changeInput() {
         input = "changed";
     }
+
 }
