3199ae6beb57f17bed0f8e8ef713fc72e93c5cb9
TajBanana <ytahjian@gmail.com>
2021-12-13 22:53:50 +0800

    basic files for socket programming

--- src/main/java/SingleClient.java
+++ src/main/java/SingleClient.java
@@ -1,2 +1,10 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class SingleClient {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket =  new Socket("localhost", 12345);
+
+    }
 }
--- src/main/java/SingleServer.java
+++ src/main/java/SingleServer.java
@@ -1,9 +1,15 @@
-public class SingleServer {
-    public static void main(String[] args) {
-        method1();
-    }
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-    public static void method1() {
-        System.out.println("this is a program");
+public class SingleServer {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(12345);
+        Socket socket = server.accept();    //blocking function for the server
+        String msg = "";
+
+        System.out.println("Listening for connection at port 12345");
+
     }
 }
